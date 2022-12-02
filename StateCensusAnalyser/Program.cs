c1005adc8d1761fc618f2fd28191cda471bb2b95
rahul01r <rrdubeynibm@gmail.com>
2022-12-02 23:06:17 +0530

    [rahul] Add:UC1-IndianStatesCensusAnalyser

---
+++ StateCensusAnalyser/Program.cs
@@ -0,0 +1,29 @@
+﻿using StateCensusAnalyser.DTO;
+using StateCensusAnalyser.UsingCode;
+
+namespace StateCensusAnalyser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to IndianStatesCensusAnalyser ");
+            string filePathStateCensus = @"C:\Users\Lenovo\Desktop\Git\IndianStateCensusAnalyzer\StateCensusAnalyser\DTO\StateCensus.csv";
+            string filePath = @"C:\Users\Lenovo\Desktop\Git\IndianStateCensusAnalyzer\StateCensusAnalyser\UsingCode\StateCode.csv";
+
+            StateCensusAnalyzer analyzer = new StateCensusAnalyzer();
+            int StateCensusRecords = analyzer.ReadStateCensusData(filePathStateCensus);
+            CSVStateCensus census = new CSVStateCensus();
+            int censusRecords = census.ReadStateCensusData(filePathStateCensus);
+            if (StateCensusRecords == censusRecords)
+                Console.WriteLine("Number of records match");
+
+            StateCodeAnalyzer stateCodeAnalyzer = new StateCodeAnalyzer();
+            int StateCodeRecords = stateCodeAnalyzer.ReadStateCodeData(filePath);
+            CSVStateCensus stateCode = new CSVStateCensus();
+            int codeRecords = stateCode.ReadStateCodeData(filePath);
+            if (StateCodeRecords == codeRecords)
+                Console.WriteLine("Number of records match");
+        }
+    }
+}
